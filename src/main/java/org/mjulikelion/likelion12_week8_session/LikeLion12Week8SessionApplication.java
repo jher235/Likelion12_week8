1e3afb73b695e26a4e336f07346849152430b018
jher235 <tim668666@gmail.com>
2024-05-25 15:32:43 +0900

    setting: springboot생성 - #4

---
+++ src/main/java/org/mjulikelion/likelion12_week8_session/LikeLion12Week8SessionApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12_week8_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLion12Week8SessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeLion12Week8SessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mjulikelion/likelion12_week8_session/LikeLion12Week8SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12_week8_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLion12Week8SessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
