1f81f9a9451b498c9d68fcd7f67880506e3690a5
jher235 <tim668666@gmail.com>
2024-05-25 15:50:26 +0900

    feat: 컨트롤러 생성 ãJh - #2

---
+++ src/main/java/org/mjulikelion/likelion12_week8_session/controller/KjhController.java
@@ -0,0 +1,8 @@
+package org.mjulikelion.likelion12_week8_session.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KjhController {
+}
